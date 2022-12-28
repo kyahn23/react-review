2bc902c39ff7d2b54efec7dac1c88618e73afdb6
kyahn23 <kyahn23@gmail.com>
2022-12-29 03:16:41 +0900

    review-useRef,restAPI

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,28 @@
+import { useNavigate } from "react-router-dom";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage() {
-  return <div>New Meetup Page</div>;
+  const navigate = useNavigate();
+  const addMeetupHandler = (meetupData) => {
+    fetch(
+      "https://react-review-a9e2f-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      navigate("/");
+    });
+  };
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 
 export default NewMeetupPage;
